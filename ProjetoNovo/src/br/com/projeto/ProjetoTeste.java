81ed9a9823f3df07a39e5e6706f88e7ef09f8e23
kelsilva <kellogical@hotmail.com>
2017-01-24 17:54:36 -0300

    Update ProjetoTeste.java

--- ProjetoNovo/src/br/com/projeto/ProjetoTeste.java
+++ ProjetoNovo/src/br/com/projeto/ProjetoTeste.java
@@ -4,7 +4,7 @@ public class ProjetoTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("teste inicial");
+  System.out.println("teste inicial...upload");
 	}
 
 }
