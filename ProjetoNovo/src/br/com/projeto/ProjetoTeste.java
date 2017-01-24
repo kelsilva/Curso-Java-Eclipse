68a335613ad43e195114f4c52f3a0c154cdc8692
kelsilva <kellogical@hotmail.com>
2017-01-24 17:52:54 -0300

    Teste de inicio

---
+++ ProjetoNovo/src/br/com/projeto/ProjetoTeste.java
@@ -0,0 +1,10 @@
+package br.com.projeto;
+
+public class ProjetoTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("teste inicial");
+	}
+
+}
