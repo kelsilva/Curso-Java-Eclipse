e0940d4c5e7a5c96f4a9d293ac5ffa22d67e277a
Kel <Kel@Kel-PC>
2017-01-26 18:32:10 -0300

    ola mundo segundo

--- ProjetoNovo/src/br/com/projeto/OlaMundo.java
+++ ProjetoNovo/src/br/com/projeto/OlaMundo.java
@@ -5,6 +5,7 @@ public class OlaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("ola mundo");
+		System.out.println ("ola mundo segundo ");
 
 	}
 
