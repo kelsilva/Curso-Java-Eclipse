6cc8029c0af7a57a3c89c3b380af8f98b7f0cce3
Kel <Kel@Kel-PC>
2017-01-26 00:53:40 -0300

    enviando

--- ProjetoNovo/src/br/com/projeto/ProjetoTeste.java
+++ ProjetoNovo/src/br/com/projeto/ProjetoTeste.java
@@ -4,7 +4,7 @@ public class ProjetoTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("TESTE FINAL...........");
+  System.out.println("TESTE FINAL...........EVIANDO");
 	}
 
 }
